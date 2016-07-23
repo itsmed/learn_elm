7a7a8ef5456c6e7f7f28eb4012a2093d2c168531
DejonGill <dejonjgill@gmail.com>
2016-07-23 10:30:21 -0700

    Validating all input types

--- baiscForms.elm
+++ baiscForms.elm
@@ -16,12 +16,13 @@ type alias Model =
   { name : String
   , password : String
   , passwordAgain: String
+  , age: String
   }
 
 model : Model
 
 model =
-  Model "" "" ""
+  Model "" "" "" ""
 
 
 -- UPDATE
@@ -30,6 +31,7 @@ type Msg
   = Name String
   | Password String
   | PasswordAgain String
+  | Age String
 
 
 update : Msg -> Model -> Model
@@ -45,6 +47,9 @@ update msg model =
     PasswordAgain password -> 
       { model | passwordAgain = password }
 
+    Age age ->
+      { model | age = age }
+
 -- VIEW
 
 view : Model -> Html Msg
@@ -54,21 +59,96 @@ view model =
   [ input [ type' "text", placeholder "Name", onInput Name ] []
   , input [ type' "password", placeholder "Password", onInput Password ] []
   , input [ type' "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
-  , viewValidation model
+  , input [ type' "number", placeholder "Age", onInput Age ] []
+  , button [ type' "submit" ] [ text "Submit" ]
+  , passwordLengthValidation model
+  , passwordMatchValidation model
+  , passwordContainsNumbersValidation model
+  , passwordContainsLowerCaseValidation model
+  , passwordContainsUpperCaseValidation model
+  , ageIsNumberValidation model
+  --, validateAll
   ]
 
-viewValidation : Model -> Html Msg
-viewValidation model =
+
+
+
+passwordLengthValidation : Model -> Html Msg
+passwordLengthValidation model =
 
   let
     (color, message) =
---"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}"
+
       if String.length model.password >= 8 then 
-        ("green", "Contains j")
-      else if model.password == model.passwordAgain then
-        ("green", "matches")
+        ("green", "Long Enough")
       else 
-        ("red", "something is wrong")
+        ("red", "Not enough characters")
       
   in 
     div [ style [("color", color)] ] [ text message ] 
+
+passwordMatchValidation : Model -> Html Msg
+passwordMatchValidation model =
+
+  let
+    (color, message) =
+      if model.password == model.passwordAgain then
+        ("green", "matches")
+      else
+        ("red", "passwords don't match")
+
+    in
+      div [ style [("color", color)] ] [ text message ]
+
+passwordContainsNumbersValidation: Model -> Html Msg
+passwordContainsNumbersValidation model =
+
+  let
+    (color, message) =
+      if Regex.contains (regex "[0-9]") model.password then
+        ("green", "contains numbers")
+      else
+        ("red", "password must contain numbers")
+
+    in
+      div [ style [("color", color)] ] [ text message ]
+
+
+passwordContainsLowerCaseValidation: Model -> Html Msg
+passwordContainsLowerCaseValidation model =
+
+  let
+    (color, message) =
+      if Regex.contains (regex "[a-z]") model.password then
+        ("green", "contains lower case letters")
+      else
+        ("red", "pasword must contain lower case letters")
+
+    in
+      div [ style [("color", color)] ] [ text message ]
+
+passwordContainsUpperCaseValidation: Model -> Html Msg
+passwordContainsUpperCaseValidation model =
+
+  let
+    (color, message) =
+      if Regex.contains (regex "[A-Z]") model.password then
+        ("green", "contains upper case letters")
+      else
+        ("red", "pasword must contain upper case letters")
+
+    in
+      div [ style [("color", color)] ] [ text message ]
+
+ageIsNumberValidation: Model -> Html Msg
+ageIsNumberValidation model =
+
+  let
+    (color, message) =
+      if Regex.contains (regex "[0-9]") model.age then
+        ("green", "age ain't nothin' but a number")
+      else
+        ("red", "age must be a number")
+
+    in
+      div [ style [("color", color)] ] [ text message ]
